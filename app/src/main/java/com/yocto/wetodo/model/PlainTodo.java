2e73777b08c971b51dce1ebd3fc9ba518bdc263f
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2020-05-25 16:01:21 +0800

    WIP

--- app/src/main/java/com/yocto/wetodo/model/PlainTodo.java
+++ app/src/main/java/com/yocto/wetodo/model/PlainTodo.java
@@ -1,4 +1,60 @@
 package com.yocto.wetodo.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+    tableName = "plain_todo",
+    indices = {
+        @Index("folder")
+    }
+)
 public class PlainTodo {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private long id;
+
+    @ColumnInfo(name = "folder")
+    private String folder;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "checked")
+    private boolean checked;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFolder() {
+        return folder;
+    }
+
+    public void setFolder(String folder) {
+        this.folder = folder;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
 }
--- app/src/main/java/com/yocto/wetodo/model/Todo.java
+++ app/src/main/java/com/yocto/wetodo/model/Todo.java
@@ -1,4 +1,20 @@
 package com.yocto.wetodo.model;
 
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Todo {
+    @Embedded
+    private PlainTodo plainTodo = new PlainTodo();
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "plain_todo_id",
+        entity = SimpleTodo.class
+    )
+    private List<SimpleTodo> simpleTodos = new ArrayList<>();
+
 }
