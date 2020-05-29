a607aef155e49a7ceeb2bba8f57fb290aa1b59b7
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2020-05-29 17:31:04 +0800

    WIP

--- app/src/main/java/com/yocto/wetodo/repository/TodoRepository.java
+++ app/src/main/java/com/yocto/wetodo/repository/TodoRepository.java
@@ -4,11 +4,23 @@ import androidx.paging.DataSource;
 
 import com.yocto.wetodo.model.Todo;
 
+import java.util.List;
+
 public enum TodoRepository {
     INSTANCE;
 
-    public DataSource.Factory<Integer, Todo> getTodos() {
+    public DataSource.Factory<Integer, Todo> getTodosDataSourceFactory() {
+        TodoDao todoDao = WeTodoRoomDatabase.instance().todoDao();
+        return todoDao.getTodosDataSourceFactory();
+    }
+
+    public List<Todo> getTodos() {
         TodoDao todoDao = WeTodoRoomDatabase.instance().todoDao();
         return todoDao.getTodos();
+    }
+
+    public long insert(Todo todo) {
+        TodoDao todoDao = WeTodoRoomDatabase.instance().todoDao();
+        return todoDao.insert(todo);
     }
 }
