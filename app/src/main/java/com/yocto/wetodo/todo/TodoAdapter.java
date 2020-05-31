1f0b8ac19c177ad64d92133c71b9bfd68e6d1a44
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2020-05-31 23:12:43 +0800

    WIP

--- app/src/main/java/com/yocto/wetodo/todo/TodoAdapter.java
+++ app/src/main/java/com/yocto/wetodo/todo/TodoAdapter.java
@@ -1,29 +1,58 @@
 package com.yocto.wetodo.todo;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.paging.PagedListAdapter;
 import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.yocto.wetodo.R;
 import com.yocto.wetodo.Utils;
 import com.yocto.wetodo.model.PlainTodo;
 import com.yocto.wetodo.model.Todo;
 
-public class TodoAdapter extends PagedListAdapter<Todo, TodoViewHolder> {
-    protected TodoAdapter(@NonNull DiffUtil.ItemCallback<Todo> diffCallback) {
-        super(diffCallback);
+public class TodoAdapter extends PagedListAdapter<Todo, TodoAdapter.ViewHolder> {
+    protected TodoAdapter() {
+        super(DIFF_CALLBACK);
     }
 
     @NonNull
     @Override
-    public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public TodoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.todo_adapter, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TodoViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull TodoAdapter.ViewHolder holder, int position) {
+        Todo todo = getItem(position);
 
+        if (todo == null) {
+            return;
+        }
+
+        final TextView titleTextView = holder.titleTextView;
+
+        titleTextView.setText(todo.getPlainTodo().getTitle());
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView titleTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleTextView = itemView.findViewById(R.id.title_text_view);
+        }
     }
 
     private static DiffUtil.ItemCallback<Todo> DIFF_CALLBACK =
