96711c3df978ecc544e00a9815d5db8f9632d568
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2020-05-31 23:14:11 +0800

    WIP

--- app/src/main/java/com/yocto/wetodo/todo/TodoDashboardFragment.java
+++ app/src/main/java/com/yocto/wetodo/todo/TodoDashboardFragment.java
@@ -6,17 +6,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.yocto.wetodo.R;
+import com.yocto.wetodo.model.Todo;
 import com.yocto.wetodo.model.TodoFolder;
+import com.yocto.wetodo.model.TodoViewModel;
 
 public class TodoDashboardFragment extends Fragment {
     private static final String INTENT_EXTRA_TODO_FOLDER = "INTENT_EXTRA_TODO_FOLDER";
 
     private TodoFolder todoFolder;
+    private TodoViewModel todoViewModel;
 
     private RecyclerView recyclerView;
+    private TodoAdapter todoAdapter;
+
+    private final TodosPagedListObserver todosPagedListObserver = new TodosPagedListObserver();
+
+    private class TodosPagedListObserver implements Observer<PagedList<Todo>> {
+
+        @Override
+        public void onChanged(PagedList<Todo> todosPageList) {
+            todoAdapter.submitList(todosPageList);
+        }
+    }
 
     public static TodoDashboardFragment newInstance(TodoFolder todoFolder) {
         TodoDashboardFragment todoDashboardFragment = new TodoDashboardFragment();
@@ -32,6 +51,9 @@ public class TodoDashboardFragment extends Fragment {
 
         Bundle arguments = getArguments();
         this.todoFolder = arguments.getParcelable(INTENT_EXTRA_TODO_FOLDER);
+
+        final ViewModelProvider viewModelProvider = new ViewModelProvider(getActivity());
+        this.todoViewModel = viewModelProvider.get(TodoViewModel.class);
     }
 
     @Override
@@ -39,7 +61,21 @@ public class TodoDashboardFragment extends Fragment {
         View view = inflater.inflate(R.layout.todo_dashboard_fragment, container, false);
 
         this.recyclerView = view.findViewById(R.id.recycler_view);
+        this.todoAdapter = new TodoAdapter();
+
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        this.recyclerView.setAdapter(todoAdapter);
+
+        final LifecycleOwner viewLifecycleOwner = getViewLifecycleOwner();
+        this.todoViewModel.getTodosPagedListLiveData().removeObservers(viewLifecycleOwner);
+        this.todoViewModel.getTodosPagedListLiveData().observe(viewLifecycleOwner, todosPagedListObserver);
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
